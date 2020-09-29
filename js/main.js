d76ed777e2843644d39e4d45e18c7051fd865784
wonjinsong <msmk22239@gmail.com>
2020-09-29 16:17:04 -0400

    25. image blending 8

--- js/main.js
+++ js/main.js
@@ -452,8 +452,17 @@
                     //캔버스 닿은 후
                     //이미지 블랜드
                     //blendHeight : [0, 0, {start:0, end:0}]
+                    values.blendHeight[0] = 0;
+                    values.blendHeight[1] = objs.canvas.height;
+                    values.blendHeight[2].start = values.rect1X[2].end;
+                    values.blendHeight[2].end = values.blendHeight[2].start + 0.2;
+                    
+                    const blendHeight = calcValues(values.blendHeight, currentYOffset);
                     //ctx.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
-                    objs.context.drawImage(objs.images[1], 0, 200);
+                    objs.context.drawImage(objs.images[1],
+                        0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight,
+                        0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight    
+                    );
                     objs.canvas.classList.add('sticky');
                     objs.canvas.style.top = `${-(objs.canvas.height - objs.canvas.height * canvasScaleRatio) / 2}px`;
                 }
